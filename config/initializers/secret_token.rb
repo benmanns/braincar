f17d171d2c3a3d5e06cb1487336976ce0ed195d7
Benjamin Manns <benmanns@gmail.com>
2013-02-12 11:35:18 -0500

    Generate Rails 3.2.12 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainCar::Application.config.secret_token = '84e00db0ec7438fa256015be2b79af3fc354515661394d19bef67c648f294b9aa1906ec35d58a05a73bc76c8327a0ad7c7c6a98874c04e384a6dbb5f532c8c3d'
